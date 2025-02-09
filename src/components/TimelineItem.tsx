27c586bb21a74a8b7127b12f5d7872a886133e54
Markus <markusevanger@gmail.com>
2025-02-09 22:18:47 +0100

    updt

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -1,5 +1,5 @@
 import { ArrowUpRight, Download } from "lucide-react";
-import { Event, Attachment } from "./Types";
+import { Event, Attachment } from "./Types.tsx";
 
 export default function TimelineItem({ event, datePlacement, className }: { event: Event; datePlacement: "left" | "right", className: string }) {
 
